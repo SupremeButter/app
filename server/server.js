c4cba667ee8264e80ec4e775d2d3b01efb9d99cf
SupremeButter <83093090+SupremeButter@users.noreply.github.com>
2021-05-31 17:33:43 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,17 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(3000, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
 const io = require('socket.io')();
 const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
 const { FRAME_RATE } = require('./constants');
