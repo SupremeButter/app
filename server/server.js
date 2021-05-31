dd0a44766b0217479c62f48e44d12e6ecccd571d
SupremeButter <83093090+SupremeButter@users.noreply.github.com>
2021-05-31 17:58:27 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,4 @@
-const io = require('socket.io')(); {
-            websockets(app, {
-  allowEIO3: true // false by default
-});
-           }
+const io = require('socket.io')();
 const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
 const { FRAME_RATE } = require('./constants');
 const { makeid } = require('./utils');
