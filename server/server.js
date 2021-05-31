213f9b84bfb332ec349060245d966b03b56d0898
SupremeButter <83093090+SupremeButter@users.noreply.github.com>
2021-05-31 17:56:18 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,3 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
-app.use(cors())
-
-var corsOptions = {
-  origin: 'https://stoic-sinoussi-9d08f5.netlify.app/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.get('/products/:id', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
-
-app.listen(3000, function () {
-  console.log('CORS-enabled web server listening on port 3000')
-  })
 const io = require('socket.io')(); {
             websockets(app, {
   allowEIO3: true // false by default
